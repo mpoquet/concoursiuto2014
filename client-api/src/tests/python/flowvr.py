08a23d359aad98a4ec897d17c8c272c15e390a40
zephyr <zephyr@zephyr-pc.(none)>
2013-04-09 22:15:57 +0200

    Ajout d'un script utilisant le client qui flood le serveur pour lui envoyer plus de 50 Mo de données (peu être un peu lent a floodé car l'api n'est pas faite pour gérer des chaines de caractères de 50 Mo via python...)

---
+++ client-api/src/tests/python/flowvr.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Machine à flood N°1 !
+
+from contest import *
+from time import *
+
+
+def gameError(session):
+    print "ERROR:", session.lastError()
+    exit(-1)
+
+
+def gameInit(login, ip='127.0.0.1', port=4242):
+    print "Création de la sessions..."
+    session = Session()
+
+    print "Connexion..."
+    if not session.connect(ip, port):
+        gameError(session)
+
+    print "Login..."
+    if session.login(login) != LOGIN_OK:
+        gameError(session)
+
+    print "Attente de l'initialisation jeu..."
+    if not session.waitInit():
+        gameError(session)
+
+    data = session.gameData()
+    infos = data.globalInformations()
+    print "Joueur", infos.playerId, "dans une partie de", infos.playerCount, "joueurs"
+
+    return session
+
+
+def gameRound(session):
+    data = session.gameData()
+    infos = data.globalInformations()
+    planets = data.planets()
+
+    print "Avancement:", infos.currentRoundId, "/", infos.totalRoundCount, "tours"
+    print "Contrôle:", len(planets), "/", infos.planetCount, "planètes:", [i.planetId for i in planets]
+    print "Ressources:", infos.resources
+    print "Vaisseaux:", sum([i.shipCount for i in planets])
+    print
+
+    print "Flood en cours..."
+    planetFlood = planets[0].planetId
+    for i in xrange(15*1000*1000):
+        session.orderBuild(planetFlood, 1)
+
+    print "Flood terminé, envoi !"
+    session.sendOrders()
+
+
+def game():
+    session = gameInit("observer")
+
+    # pré-calculs
+
+    print "Attente du début du jeu..."
+
+    while True:
+        status = session.waitRoundStarting()
+
+        if status == ROUND_NETWORK_ERROR: gameError(session)
+        elif status == ROUND_END_OF_GAME: break
+
+        gameRound(session)
+
+    print "End of the game"
+
+
+game()
+
+
