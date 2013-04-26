a62307bec1d51abba4e9a09888563e91daee6faa
razlock <razlock@gmail.com>
2013-04-26 18:32:35 +0200

    Le générateur permet maintenant de générer un fichier de galaxy au bon format pour le serveur, il suffit de concaténer plusieurs galaxies dans un fichier .map pour avoir un univers.

---
+++ map/generator/gen44.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Generate 4 galaxies of equal size at equal distance with 16 planets in each
+./a.out galaxy1.ga 1 0 0 50 4 16
+./a.out galaxy2.ga 2 0 250 50 4 16
+./a.out galaxy3.ga 3 250 250 50 4 16
+./a.out galaxy4.ga 4 250 0 50 4 16
+
+cat galaxy1.ga > test.map
+cat galaxy2.ga >> test.map 
+cat galaxy3.ga >> test.map 
+cat galaxy4.ga >> test.map 
--- map/generator/main.cpp
+++ map/generator/main.cpp
@@ -5,102 +5,123 @@
 
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 struct planet_s
-{
-    int id;
-    int playerId;
-    
-    float angle;
-    int range;
-    
+{    
     float x, y;
+    
+    int size;
+    int galaxyId;
+    bool initial;
+    bool neutral;
 };
 
 void usage(const char *name)
 {
-    printf("usage: \t\t%s players galaxies layers planets scale\n", name);
+    printf("usage: \t\t%s file galaxy X Y radius players planets\n", name);
+    printf("map: \t\toutput file name\n");
+    printf("galaxy: \tid of the galaxy [1-N]\n");
+    printf("X, Y: \t\tposition of the galaxy [0-N], [0-N]\n");
     printf("players: \tnumber of players, [1-N]\n");
-    printf("galaxies: \tnumber of galaxies [1-N]\n");
-    printf("layers: \tnumber of layers per galaxy [1-N]\n");
-    printf("planets: \tnumber of planets per layer per player, [1-N]\n");
-    printf("scale: \tscale of the generated galaxy, [1-N]\n");
-    printf("\nExample: %s 4 4 4 4 1\n", name);
-    printf("will result in 64 planets per galaxy with 4 planets per layer per player.\n");
+    printf("planets: \tnumber of planets, [1-N]\n");
+    printf("radius: \tradius of the galaxy / 2, [1-N]\n");
+    printf("\nExample: %s test.map 1 0 0 100 4 16\n", name);
+    printf("will result in a galaxy of 16 planets with 4 planets per player.\n");
 }
 
 int main(int argc, char **argv)
 {
-    int nPlayers = -1, nGalaxies = -1, nLayers = -1, nPlanets = -1, scale = -1;
+    string filename;
+    int galaxyId = -1, X = -1, Y = -1, nPlayers = -1, nPlanets = -1, radius = -1;
     
-    if(argc < 6)
+    if(argc < 8)
     {
         usage(argv[0]);
         return 0;
     }
     
-    nPlayers = atoi(argv[1]);
-    nGalaxies = atoi(argv[2]);
-    nLayers = atoi(argv[3]);
-    nPlanets = atoi(argv[4]);
-    scale = atoi(argv[5]);
+    filename = argv[1];
+    galaxyId = atoi(argv[2]);
+    X = atoi(argv[3]);
+    Y = atoi(argv[4]);
+    radius = atoi(argv[5]);
+    nPlayers = atoi(argv[6]);
+    nPlanets = atoi(argv[7]);
     
-    if(nPlayers < 1 || nGalaxies < 1 || nLayers < 1 || nPlanets < 1 || scale < 1)
+    if(filename.empty() || galaxyId < 1 || radius < 1 || X < 0 || Y < 0 || nPlayers < 1 || nPlanets < 1)
     {
         usage(argv[0]);
         return 0;
     }
     
-    printf("Generating with: players=%d, galaxies=%d, layers=%d, planets=%d, scale=%d\n", 
-        nPlayers, nGalaxies, nLayers, nPlanets, scale);
+    printf("Generating %s with id=%d, X=%d, Y=%d, radius=%d, players=%d, planets=%d\n", 
+        filename.c_str(), galaxyId, X, Y, radius, nPlayers, nPlanets);
     
     srand(time(0));
     
-    vector<planet_s> planets(nGalaxies * nLayers * nPlayers * nPlanets);
+    int nLayers = -1;
+    int nPlanetPerPlayer = -1;
+    int nPlanetPerLayer = -1;
+    int nPlanetRest = -1;
+    
+    nLayers = rand() % 4 + 1;
+    nPlanetPerPlayer = nPlanets / nPlayers;
+    nPlanetPerLayer = nPlanetPerPlayer / nLayers;
+    nPlanetRest = nPlanets % nLayers;
+    
+    vector<planet_s> planets(nPlayers * nPlanetPerPlayer);
     
     // Pour chaque anneau de planètes,
     // on répartie les planètes équitablement
     float angle = (360 / nPlayers);
-    for(int i = 0; i < nLayers; ++i)
+
+    // On place toutes les planètes à la même distance du centre (range)
+    int id = 0;
+    int range = rand() % radius + 1;
+    for(int i = 0; i < nPlayers; ++i)
     {
-        // On place toutes les planètes à la même distance du centre (range)
-        int range = rand() % scale + 1;
-        for(int j = 0; j < nPlayers; ++j)
+        for(int j = 0; j <= nLayers; ++j)
         {
-            for(int k = 0; k < nPlanets; ++k)
+            int nPlanetInLayer = -1;
+            if(j < nLayers) nPlanetInLayer = nPlanetPerLayer;
+            else if(j == nLayers) nPlanetInLayer = nPlanetRest;
+            
+            for(int k = 0; k < nPlanetInLayer; ++k)
             {
                 struct planet_s p;
-                p.id = i * nPlayers * nPlanets + j * nPlanets + k;
-                p.playerId = j;
                 
-                // Calcule des coordonnées polaires
-                p.angle = (angle * j) + (angle / nPlanets) * k;
-                p.range = range;
-                
+                // Calcul des coordonnées polaires
+                float planetAngle = (angle * i) + (angle / nPlanetPerLayer) * k;
+                int planetRadius = range;
+
                 // Calcul des coordonnées cartésiennes
-                p.x = cos(p.angle) * range;
-                p.y = sin(p.angle) * range;
+                p.x = X + cos(planetAngle) * planetRadius + radius;
+                p.y = Y + sin(planetAngle) * planetRadius + radius;
+                p.galaxyId = galaxyId;
+                p.size = 1;
+                p.initial = false;
+                p.neutral = false;
                 
-                planets[p.id] = p;
+                planets[id++] = p;
             }
         }
+        
+        // Une planète initiale par joueur ...
+        planets[id-1].initial = true;
     }
     
-    ofstream out("test.map", ofstream::out);
+    ofstream out(filename.c_str(), ofstream::out);
     
     for(unsigned int i = 0; i < planets.size(); ++i)
     {
         planet_s &p = planets[i];
         
-        printf("id=%d, playerId=%d, angle=%f, range=%d, x=%f, y=%f\n", 
-            p.id, p.playerId, p.angle, p.range, p.x, p.y);
-        
         // write map
-        out << p.id << " " << p.x << " " << p.y << "\n";
+        out << (int)p.x << " " << (int)p.y << " " << p.size << " " << p.galaxyId << " " << p.initial << " " << p.neutral << "\n";
     }
-    out << "\n";
     
     out.close();
     
